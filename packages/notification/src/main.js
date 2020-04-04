66f8cd9ae5ffa5fc45ea4df9fe0e343d33bd6eca
baimin <xiaofengbm@gmail.com>
2020-04-04 22:08:08 +0800

    feat(element): notify组件(一)

--- packages/notification/src/main.js
+++ packages/notification/src/main.js
@@ -1,47 +1,77 @@
 import Vue from 'vue';
 import Main from './main.vue';
-import merge from '@/utils/merge.js';
-
+// 这里不用多说了吧，看完上面的你应该了解其作用了
 const NotificationConstructor = Vue.extend(Main);
 
-let notify;                 // 单个弹框实例
-let notify_list = [];       // 存放多个弹框实例
-let seed = 1;               // 计数id
+// 当前实例对象，创建在Notification函数外面，是因为在Notification外要用到。
+let instance;
+let instances = [];
+let seed = 1;
+
 
 const Notification = function(options) {
-  // 当前 Vue 实例是否运行于服务器。(Vue源码中定义在prototype上的)
-  if(Vue.prototype.$isServer) return;
-  options = merge({}, options);
+
+  // 将我们传入的options拷贝一份。
+  // 这里merge方法作用和Object.assign()相同，所以这里为了简化，就用Object.assign替代
+  options = Object.assign({}, options);
+
+  const userOnClose = options.onClose;
+  const id = 'notification_' + seed++;
+  const position = options.position || 'top-right';
 
   options.onClose = function() {
-    notify_list.pop();
-    console.log(notify_list)
-  }
+    Notification.close(id, userOnClose);
+  };
 
-  // 创建notify实例
-  notify = new NotificationConstructor({
+  instance = new NotificationConstructor({
     data: options
   });
+  instance.id = id;
   // 挂载
-  notify.$mount();
-  document.body.appendChild(notify.$el);
-  // 设置visible为true
-  notify.visible = true;
+  instance.$mount();
+  // $el是组件生成的dom对象
+  document.body.appendChild(instance.$el);
+  // 实例添加visible属性，控制显示
+  instance.visible = true;
+  instance.dom = instance.$el;
 
+  // offset是用户设置的偏移距离，不知道的看一下element-ui的文档
   let verticalOffset = options.offset || 0;
-  notify_list.forEach(item => {
+  // instances是一个数组，用来存放当前所有生成的notify组件实例
+  instances.filter(item => item.position === position).forEach(item => {
+    // 当前组件偏移的位置，不明白的可以看下图
     verticalOffset += item.$el.offsetHeight + 16;
-  })
+  });
   verticalOffset += 16;
-  notify.verticalOffset = verticalOffset;
-  notify_list.push(notify);
-  return notify;
-}
-
+  // 设置当前实例的偏移距离
+  instance.verticalOffset = verticalOffset;
+  instances.push(instance);
+  return instance;
+};
+ 
 Notification.close = function(id, userOnClose) {
+  let index = -1;
+  const len = instances.length;
+  const instance = instances.filter((instance, i) => {
+    if (instance.id === id) {
+      index = i;
+      return true;
+    }
+    return false;
+  })[0];
 
+
+  instances.splice(index, 1);
+
+
+  const position = instance.position;
+  const removedHeight = instance.dom.offsetHeight;
+  for (let i = index; i < len - 1 ; i++) {
+    if (instances[i].position === position) {
+      instances[i].dom.style[instance.verticalProperty] =
+        parseInt(instances[i].dom.style[instance.verticalProperty], 10) - removedHeight - 16 + 'px';
+    }
+  }
 }
-
-
 
 export default Notification;
--- packages/notification/src/main.vue
+++ packages/notification/src/main.vue
@@ -1,11 +1,11 @@
 <template>
+
   <transition name="el-notification-fade">
     <div 
-      :class="['el-notification', horizontalClass]" 
+      :class="['el-notification', horizontalClass]"
       :style="positionStyle"
       v-show="visible"
     >
-      <i class="el-notification__icon" :class="[ typeClass ]"></i>
       <div class="el-notification__group">
         <h2 class="el-notification__title" v-text="title"></h2>
         <div class="el-notification__content" v-show="message">
@@ -13,53 +13,69 @@
             <p>{{ message }}</p>
           </slot>
         </div>
-        <!-- 关闭按钮 -->
-        <div class="el-notification__closeBtn el-icon-close" v-if="showClose" @click.stop="close"></div>
+
+        <div
+          class="el-notification__closeBtn el-icon-close"
+          v-if="showClose"
+          @click.stop="close"
+        ></div>
       </div>
     </div>
   </transition>
 </template>
 
 <script>
-let typeMap = {
-  success: "success",
-  info: "info",
-  warning: "warning",
-  error: "error"
-};
+
 export default {
   data() {
     return {
       visible: false,
-      title: "",
-      message: "",
-      type: "",
       showClose: true,
-      onClose: null,
-      position: "top-right",
+      closed: false,
+      title: '',
+      message: '',
+      position: 'top-right',
       verticalOffset: 0
-    };
+    }
   },
   computed: {
-    typeClass() {
-      return this.type && typeMap[this.type] ? `el-icon-${ typeMap[this.type] }` : '';
-    },
     horizontalClass() {
       return this.position.indexOf('right') > -1 ? 'right' : 'left';
     },
+    // 在computed添加positionStyle以及verticalProperty
+    verticalProperty() {
+      return /^top-/.test(this.position) ? 'top' : 'bottom';
+    },
     positionStyle() {
+      // 值源码中是通过verticalOffset设置，通过main.js计算出偏移位置，这里为了演示方便，先默认写死0px
       return {
-        top: `${ this.verticalOffset }px`
-      };
+        [this.verticalProperty]: `${this.verticalOffset}px`
+      }
     }
   },
-  methods: {
-    close() {
-      this.onClose();
-    }
-  },
-  mounted() {
 
+  watch: {
+    closed(newVal) {
+      if (newVal) {
+        this.visible = false;
+        this.$el.addEventListener('transitionend', this.destroyElement);
+        
+      }
+    }
+  },
+
+  methods: {
+    destroyElement() {
+      this.$el.removeEventListener('transitionend', this.destroyElement);
+      this.$destroy(true);
+      this.$el.parentNode.removeChild(this.$el);
+    },
+    close() {
+      this.closed = true;
+      if (typeof this.onClose === 'function') {
+        this.onClose();
+      }
+    },
   }
 };
 </script>
